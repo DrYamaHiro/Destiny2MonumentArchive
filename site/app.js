3d69e1408277c083b3cf45631b1f65509b1ab35f
DrYamaHiro <dryamahiro@users.noreply.github.com>
2026-06-16 22:48:42 +0900

    Remove internal scrolling from weapon perk selectors

--- site/app.js
+++ site/app.js
@@ -1,5 +1,5 @@
 const LIMIT = 250;
-const DATA_VERSION = "20260616-expanded-perk-grid";
+const DATA_VERSION = "20260616-no-internal-plug-scroll";
 
 const finalReleaseVersions = new Set(["v950", "v960", "v970"]);
 const finalReleaseSeasonNumbers = new Set([29]);
